d3be967b55f4bf2e5b85ea2e36869ea8dcb34be2
Yura Kalemi <ykalemi@naumen.ru>
2017-10-11 14:47:05 +0500

    repackage for WAR

--- src/main/java/ru/naumen/perfhouse/PerfhouseApplication.java
+++ src/main/java/ru/naumen/perfhouse/PerfhouseApplication.java
@@ -6,11 +6,19 @@ import java.text.ParseException;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import ru.naumen.sd40.log.parser.App;
 
 @SpringBootApplication(scanBasePackages = { "ru.naumen" })
-public class PerfhouseApplication
+public class PerfhouseApplication extends SpringBootServletInitializer
 {
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(PerfhouseApplication.class);
+    }
+
+
     public static void main(String[] args) throws IOException, ParseException
     {
         if (System.getProperty("Parser") != null)
